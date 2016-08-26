01db396e05ba07c851921704a7658699715959ed
AUTOTRADER\dpamidi <divya.pamidi@vinsolutions.com>
2016-08-26 15:41:20 -0500

    adding taskitem.scss

--- src/components/widgets/TaskItem.js
+++ src/components/widgets/TaskItem.js
@@ -1,132 +1,66 @@
 import React, {Component, PropTypes} from 'react';
-import { Button } from 'react-bootstrap';
-
-function getStyles() {
-  const styles = {
-    root: {
-      display: 'block',
-      fontSize: 16,
-      lineHeight: '16px',
-      position: 'relative',
-      border: '1px solid #ddd',
-      background: '#fff',
-      height: '90px',
-      width: '95%',
-    },
-
-    leftStyle:{
-      marginTop:'20px',
-      marginLeft:'40px',
-      float: 'left',
-    },
-
-    textStyle:{
-      marginTop:'12px',
-      marginLeft:'30px',
-      float: 'left'
-    },
-
-    icons: {
-      height: 24,
-      width: 24,
-      display: 'block',
-      margin: 12,
-    },
-
-    leftIcon: {
-      left: 4,
-    },
-
-    label: {
-      cursor: 'pointer',
-    },
-
-    primaryText: {
-      marginTop: 0,
-      textAlign: 'left',
-      fontWeight: 'bold',
-      marginBottom: 4
-    },
-
-    Button:{
-      MarginTop: 32,
-      float: 'Right',
-      padding: '12px',
-      paddingTop: 20,
-    },
-
-    secondaryText: {
-      fontStyle: 'italic',
-      marginTop: 0,
-      textAlign: 'left',
-      marginBottom: 4
-    },
-
-    alert:{
-      height:90,
-      borderLeft: '5px solid red',
-      marginLeft:'0px',
-      float: 'left',
-      position: 'relative',
-    },
-  };
-  return styles;
-}
+import { Button, ListGroupItem } from 'react-bootstrap';
 
 class TaskItem extends Component {
   constructor(props) {
     super(props);
   }
 
-  pushElement(addIcon, element, baseStyles) {
+  pushElement(addIcon, element) {
     if (element) {
-      const styles = Object.assign({}, baseStyles, element.props.style);
       addIcon.push(
         React.cloneElement(element, {
           key: addIcon.length,
-          style: styles,
+          style: {height: '24px', width: '24px',display: 'block', margin: '12px'}
         })
       );
     }
   }
 
-  createTextElement( styles, className, data, key) {
-    return (
-      <span key={key} style={{marginTop:0}}>
-       <p style={styles} className={className}> {data} </p>
+  createTextElement(className, data, key) {
+    if(key === "primaryText"){
+      return (
+      <span key={key}>
+       <h5 className={className}> {data} </h5>
       </span>
-    );
+      );
+    }
+    else {
+      return (
+        <span key={key}>
+       <p className={className}> {data} </p>
+      </span>
+      );
+    }
   }
 
-  createButtonElement(styles, label, key) {
+  createButtonElement(label, key) {
     return (
-      <span key={key} style={styles}>
+      <span key={key} >
       <Button bsStyle="primary" >
         <span>{label}</span>
       </Button>
       </span>
     );
   }
-  createAlert(styles, key){
+
+  createAlert(className, key){
     return(
-    <span key={key} style={styles} />
+    <span key={key} className={className} />
     );
   }
-
 
   render() {
     const {
       children,
       primaryText,
       secondaryText,
-      style,
       leftIcon,
       taskButtonlabel,
       typeText,
       alert
     } = this.props;
 
-    const styles = getStyles(this.props, this.state);
     const addIcon = [];
     const addButton = [];
     const addText = [children];
@@ -134,7 +68,7 @@ class TaskItem extends Component {
 
     if(alert){
       const addAlert = this.createAlert(
-        styles.alert,
+       'taskitem__alert',
         'alert'
       );
       alertspan.push(addAlert);
@@ -143,25 +77,22 @@ class TaskItem extends Component {
     if (leftIcon) {
       this.pushElement(
         addIcon,
-        leftIcon,
-        Object.assign({}, styles.icons, styles.leftIcon)
-        );
+        leftIcon
+      );
     }
 
     if(taskButtonlabel){
       const buttonElement = this.createButtonElement(
-      styles.Button,
-      taskButtonlabel,
-      'taskButton'
+       taskButtonlabel,
+       'taskButton'
     );
       addButton.push(buttonElement);
     }
 
     if (primaryText) {
       const primaryTextElement = this.createTextElement(
-        styles.primaryText,
-        '',
-        primaryText,
+        "list-group-item-text taskitem__text",
+         primaryText,
         'primaryText'
       );
       addText.push(primaryTextElement);
@@ -169,47 +100,42 @@ class TaskItem extends Component {
 
     if (secondaryText) {
       const secondaryTextElement = this.createTextElement(
-        styles.secondaryText,
-        "text-muted",
-        secondaryText,
+        "list-group-item-text text-muted",
+         secondaryText,
         'secondaryText'
       );
       addText.push(secondaryTextElement);
     }
     if (typeText) {
       const primaryTextElement = this.createTextElement(
-        styles.secondaryText,
-        "text-muted",
-        typeText,
+        "list-group-item-text text-muted",
+         typeText,
         'typeText'
       );
       addText.push(primaryTextElement);
     }
 
     return (
-
-      <center>
-        <div style={Object.assign(styles.root, style)}>
+    <div className="taskitem">
+        <ListGroupItem className="taskitem__itemcontainer">
           {alertspan}
-          <span style={styles.leftStyle}>
+          <span className="taskitem__left">
           {addIcon}
           </span>
-          <span style={styles.textStyle}>
+          <span className="taskitem__textposition">
           {addText}
-          </span>
-          {addButton}
-          <span style={styles.Button}>
-          <Button>
-            <span>Dismiss</span>
-          </Button>
            </span>
-          <span style={styles.Button}>
-          <Button>
-            <span>Edit</span>
-          </Button></span>
-        </div>
-      </center>
-
+          <span className="taskitem__button">
+          {addButton}
+          </span>
+          <span className="taskitem__button">
+          <Button><span>Dismiss</span></Button>
+           </span>
+          <span className="taskitem__button">
+          <Button><span>Edit</span></Button>
+           </span>
+          </ListGroupItem>
+  </div>
     );
   }
 }
