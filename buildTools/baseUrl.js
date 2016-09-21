9ee260a2cda73c16ec19de6ea37fd2706dcd760a
Cory House <housecor@gmail.com>
2016-09-21 11:20:03 -0500

    Run mock api on different port than browsersync

--- buildTools/baseUrl.js
+++ buildTools/baseUrl.js
@@ -7,7 +7,7 @@ export default function getBaseUrl() {
     if (useMockAPI) {
       // Use mock data stored in api/db.json.
       // Port configured in start-mockapi script in package.json
-      return 'http://localhost:3001';
+      return 'http://localhost:3002';
     } else {
       // Note that this baseUrl assumes you're running Vinconnect APIs
       // via IIS at vinconnect.com via a hosts file entry.
