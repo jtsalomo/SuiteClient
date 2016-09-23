71d5fa708cb1b8024ea5940f039befd0778ed233
JoJo <joseph.salomone@dealertrack.com>
2016-09-23 10:27:12 -0700

    Clean up

--- src/components/pages/FinanceDriverShort.js
+++ src/components/pages/FinanceDriverShort.js
@@ -5,12 +5,18 @@ import { setApplicantName } from '../../actions/applicant/applicantActions';
 
 import { FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
 
-const FieldGroup = ({ id, label, className, validation, help, ...props }) => {
+const ROOT_ID = 'financeDriverShort';
+const APPLICANT_FIRST_NAME = 'applicant.name.first';
+const APPLICANT_MIDDLE_NAME = 'applicant.name.middle';
+const APPLICANT_LAST_NAME = 'applicant.name.last';
+
+
+const FieldGroup = ({ id, label, className, validation, ...props }) => {
   return (
-    <FormGroup className={className} controlId={id} validationState={validation}>
+    <FormGroup className={className} controlId={id} validationState={validation === null ? null : validation === "" ? "success" : "error"}>
       <ControlLabel>{label}</ControlLabel>
       <FormControl {...props} />
-      {help && <HelpBlock>{help}</HelpBlock>}
+      {validation && <HelpBlock>{validation}</HelpBlock>}
     </FormGroup>
   );
 };
@@ -18,42 +24,64 @@ const FieldGroup = ({ id, label, className, validation, help, ...props }) => {
 class FDShort extends React.Component {
   constructor(props) {
     super(props);
-    this.onChange = this.onChange.bind(this);
-    this.getValidationState = this.getValidationState.bind(this);
+    this.getValue = this.getValue.bind(this);
+    this.setValue = this.setValue.bind(this);
+    this.getState = this.getState.bind(this);
+    this.setState = this.setState.bind(this);
+    this.validate = this.validate.bind(this);
+    this.state = {};
   }
 
-  createId(id, includeWithNoBaseId = true) {
-    return (this.props && this.props.id && (this.props.id + '.' + id)) || (includeWithNoBaseId && id);
+  createId(baseId, id, includeWithNoBaseId = true) {
+    return (baseId && (baseId + '.' + id)) || (includeWithNoBaseId && id);
   }
 
-  getValidationState(stateId) {
-    /*if (this.state[stateId].length === 10) {
-      return 'success';
-    }*/
+  getValue(type) {
+    return this.props[type];
   }
 
-  onChange(e, stateId) {
-    const state = {};
-    if (stateId === 'firstName') {
-      this.props.onFirstNameChange(e.target.value);
-    } else {
-      state[stateId] = e.target.value;
-      this.setState(
-        state
-      );
+  setValue(type, value) {
+    this.setState(type, true);
+    switch(type) {
+      case APPLICANT_FIRST_NAME:
+        this.props.onFirstNameChange(value);
+        break;
+      case APPLICANT_MIDDLE_NAME:
+        this.props.onMiddleNameChange(value);
+        break;
+      case APPLICANT_LAST_NAME:
+        this.props.onLastNameChange(value);
+        break;
+    }
+  }
+
+  getState(type) {
+    return this.state[type];
+  }
+
+  setState(type, value) {
+    super.setState({ [type]: value });
+  }
+
+  validate(type) {
+    switch(type) {
+      case APPLICANT_FIRST_NAME:
+      case APPLICANT_MIDDLE_NAME:
+      case APPLICANT_LAST_NAME:
+        return this.getState(type) === undefined ? null : this.getValue(type).length < 1 ? "Field is required" : "";
     }
   }
 
   render() {
     return (
-      <div id='FinanceDriverShort' className='FinanceDriver FinanceDriverShort'>
+      <div id={ROOT_ID} className='FinanceDriver FinanceDriverShort'>
         <form>
-          <FieldGroup id={this.createId('FinanceDriverShort.Name.First')} label='First Name' className='Name FirstName' type='text' help='Your First Name, dumbo!'
-            value={this.props.applicant.name.first} onChange={(e) => this.props.onFirstNameChange(e.target.value)} validation={this.getValidationState('firstName')} />
-          <FieldGroup id={this.createId('FinanceDriverShort.Name.Middle')} label='Middle Name' className='Name MiddleName' type='text'
-            value={this.props.applicant.name.first} onChange={(e) => this.onChange(e, 'middleName')} validation={this.getValidationState('middleName')} />
-          <FieldGroup id={this.createId('FinanceDriverShort.Name.Last')} label='Last Name' className='Name LastName' type='text'
-            value={this.props.applicant.name.first} onChange={(e) => this.onChange(e, 'lastName')} validation={this.getValidationState('lastName')} />
+          <FieldGroup id={this.createId(ROOT_ID, APPLICANT_FIRST_NAME)} label='First Name' className='Name FirstName' type='text'
+            value={this.getValue(APPLICANT_FIRST_NAME)} onChange={(e) => this.setValue(APPLICANT_FIRST_NAME, e.target.value)} validation={this.validate(APPLICANT_FIRST_NAME)} />
+          <FieldGroup id={this.createId(ROOT_ID, APPLICANT_MIDDLE_NAME)} label='Middle Name' className='Name MiddleName' type='text'
+            value={this.getValue(APPLICANT_MIDDLE_NAME)} onChange={(e) => this.setValue(APPLICANT_MIDDLE_NAME, e.target.value)} validation={this.validate(APPLICANT_MIDDLE_NAME)} />
+          <FieldGroup id={this.createId(ROOT_ID, APPLICANT_LAST_NAME)} label='Last Name' className='Name LastName' type='text'
+            value={this.getValue(APPLICANT_LAST_NAME)} onChange={(e) => this.setValue(APPLICANT_LAST_NAME, e.target.value)} validation={this.validate(APPLICANT_LAST_NAME)} />
         </form>
       </div>
     );
@@ -64,18 +92,19 @@ class FDShort extends React.Component {
 
 FDShort.propTypes = {
   id: PropTypes.string,
-  applicant: PropTypes.shape({
-    name: PropTypes.shape({
-      first: PropTypes.string
-    })
-  }),
-  onFirstNameChange: PropTypes.func
+  [APPLICANT_FIRST_NAME]: PropTypes.string,
+  [APPLICANT_MIDDLE_NAME]: PropTypes.string,
+  [APPLICANT_LAST_NAME]: PropTypes.string,
+  onFirstNameChange: PropTypes.func,
+  onMiddleNameChange: PropTypes.func,
+  onLastNameChange: PropTypes.func
 };
 
 const mapStateToProps = (state) => {
   return {
-    applicant: state.applicant,
-    firstName: state.applicant.name.first
+    [APPLICANT_FIRST_NAME]: state.applicant.name.first,
+    [APPLICANT_MIDDLE_NAME]: state.applicant.name.middle,
+    [APPLICANT_LAST_NAME]: state.applicant.name.last,
   };
 };
 
@@ -83,6 +112,12 @@ const mapDispatchToProps = (dispatch) => {
   return {
     onFirstNameChange: (firstName) => {
       dispatch(setApplicantName({ first: firstName }));
+    },
+    onMiddleNameChange: (middleName) => {
+      dispatch(setApplicantName({ middle: middleName }));
+    },
+    onLastNameChange: (lastName) => {
+      dispatch(setApplicantName({ last: lastName }));
     }
   };
 };
